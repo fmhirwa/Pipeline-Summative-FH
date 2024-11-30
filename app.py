0476292cd4a5158cd70ceec56bc424ad7653073d
fmhirwa <m.hirwa@alustudent.com>
2024-11-30 16:26:26 +0200

    Change port

--- app.py
+++ app.py
@@ -147,4 +147,6 @@ def evaluate():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT from environment variables
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=False)
